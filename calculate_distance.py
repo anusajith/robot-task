e76e1f06c887a2dc6542be3bceb9f42f28bd8b97
Anu <anusajith90@gmail.com>
2021-07-29 12:49:50 +1000

    First_skeleton

---
+++ calculate_distance.py
@@ -0,0 +1,20 @@
+class Robot:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def turn(self):
+        pass
+
+    def validate_commands(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    robo = Robot()
+    robo.main()

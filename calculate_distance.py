920ccfad66af8540b15b83b1e041dd5bd0ec834f
Anu <anusajith90@gmail.com>
2021-07-29 13:16:45 +1000

    Added functions to calculate, move and turn and set variables

--- calculate_distance.py
+++ calculate_distance.py
@@ -1,15 +1,44 @@
 class Robot:
-    def __init__(self):
-        pass
+    def __init__(self, start_x=0, start_y=0, direction="N"):
+        self.direction = direction
+        self.start_x = start_x
+        self.start_y = start_y
+        self.new_x = start_x
+        self.new_y = start_y
 
     def move(self, command):
-        pass
+        if command[0] == "F":
+            if self.direction == "N":
+                self.new_y = self.new_y + int(command[1])
+            elif self.direction == "S":
+                self.new_y = self.new_y - int(command[1])
+            elif self.direction == "E":
+                self.new_x = self.new_x + int(command[1])
+            elif self.direction == "W":
+                self.new_x = self.new_x + int(command[1])
+        else:
+            if self.direction == "N":
+                self.new_y = self.new_y - int(command[1])
+            elif self.direction == "S":
+                self.new_y = self.new_y + int(command[1])
+            elif self.direction == "E":
+                self.new_x = self.new_x - int(command[1])
+            elif self.direction == "W":
+                self.new_x = self.new_x + int(command[1])
 
     def turn(self, command):
-        pass
+        direction = ["N", "W", "S", "E"]
+        unit = int(command[1])
+        if command[0] == "L":
+            new_index = (direction.index(self.direction) + unit) % len(direction)
+            self.direction = direction[new_index]
+        else:
+            new_index = (direction.index(self.direction) - unit) % len(direction)
+            self.direction = direction[new_index]
 
     def displacement(self):
-        return 0
+        total_units = abs(self.start_x - self.new_x) + abs(self.start_y - self.new_y)
+        return total_units
 
     def validate_commands(self,command_list):
         for item in command_list:
@@ -33,5 +62,5 @@ class Robot:
 
 
 if __name__ == "__main__":
-    robo = Robot()
+    robo = Robot(0, 0, "N")
     robo.main("F1,L1,F3,R1,B4")

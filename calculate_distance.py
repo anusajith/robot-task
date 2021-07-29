a981a45f1deea4298a183fd79cdf66e5e51a2290
Anu <anusajith90@gmail.com>
2021-07-29 12:57:58 +1000

    Added main and validation functions

--- calculate_distance.py
+++ calculate_distance.py
@@ -2,19 +2,36 @@ class Robot:
     def __init__(self):
         pass
 
-    def move(self):
+    def move(self, command):
         pass
 
-    def turn(self):
+    def turn(self, command):
         pass
 
-    def validate_commands(self):
-        pass
+    def displacement(self):
+        return 0
 
-    def main(self):
-        pass
+    def validate_commands(self,command_list):
+        for item in command_list:
+            if item[0] in ("F", "B", "L", "R"):
+                print("Validated commands successfully")
+                return True
+            else:
+                print("Invalid Input Commands")
+                return False
+
+    def main(self, command):
+        command_list = command.split(",")
+        success = self.validate_commands(command_list)
+        if success:
+            for item in command_list:
+                if item[0] in ("F", "B"):
+                    self.move(item)
+                else:
+                    self.turn(item)
+            print(self.displacement())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     robo = Robot()
-    robo.main()
+    robo.main("F1,L1,F3,R1,B4")
